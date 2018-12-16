a5a2a8bca26f640ec39caf754ef493fc343abf08
Naoto Koyama <naoto.koyama@uhuru.jp>
2018-12-16 23:50:02 +0900

    add not pass test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe User, type: :model do
   let(:user) { create(:user) }
   describe "#full_name" do
     subject { user.full_name }
-    it { is_expected.to eq "first_name last_name" }
+    it { is_expected.to eq "first_name last_nam" }
   end
 end
